aabbf7c253fb17fa3bb03522d3c8862bed9260d2
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-01-12 20:32:47 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import SignUp from './components/SignUp';
+import SignIn from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/signup' element={<SignUp/>}/>
+        <Route exact path='/login' element={<SignIn/>}/>
+      </Routes>
     </div>
   );
 }
