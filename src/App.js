4403363c5492b717c36ae38bfdb3d01677575589
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-01-14 00:46:49 +0300

    added user, service, about and contact component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Home from './components/Home';
 import SignUp from './components/SignUp';
 import SignIn from './components/Login';
 import { useEffect, useState } from 'react';
+import ResponsiveAppBar from './components/NavBar';
+import About from './components/About';
+import ContactUs from './components/ContactUs';
+import Services from './components/Services';
 
 function App() {
   const [user, setUser]=useState(null)
@@ -17,10 +21,14 @@ function App() {
   },[])
   return (
     <div className="App">
+      <ResponsiveAppBar/>
       <Routes>
         <Route exact path='/' element={<Home user={user}/>}/>
         <Route exact path='/signup' element={<SignUp onSignUp={setUser}/>}/>
         <Route exact path='/login' element={<SignIn onLogin={setUser}/>}/>
+        <Route exact path='/about' element={<About/>}/>
+        <Route exact path='/contact' element={<ContactUs/>}/>
+        <Route exact path='/services' element={<Services/>}/>
       </Routes>
     </div>
   );
