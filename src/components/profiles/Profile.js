00317591792777a99051cbec3207d34cf240f7ab
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-01-27 14:52:36 +0300

    added Profile component

---
+++ src/components/profiles/Profile.js
@@ -0,0 +1,203 @@
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../context";
+import profile from "../../images/profile.jpg";
+
+function Profile() {
+    const [userProfile, setUserProfile] = useState({
+        first_name: '',
+        last_name: '',
+        email: '',
+        role: '',
+        phone: '',
+        address: '',
+      })
+  const { user } = useContext(AppContext);
+
+  useEffect(()=>{
+    setUserProfile({...userProfile, user})
+  },[])
+
+  if (user){
+  const fullName = `${user.first_name.charAt(0).toUpperCase()+user.first_name.substring(1)} ${user.last_name.charAt(0).toUpperCase()+user.last_name.substring(1)}`
+  return (
+    <div className="container">
+      <div className="main-body">
+        <div className="row gutters-sm">
+          <div className="col-md-4 mb-3">
+            <div className="card">
+              <div className="card-body">
+                <div className="d-flex flex-column align-items-center text-center">
+                  <img
+                    src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+                    alt="Admin"
+                    className="rounded-circle"
+                    width="150"
+                  />
+                  <div className="mt-3">
+                    <h4>{fullName} </h4>
+                    <p className="text-secondary mb-1">{user.role === "regular" ? "Service Person": "Home Owner" }</p>
+                    <p className="text-muted font-size-sm">
+                      {user.address}
+                    </p>
+                    {/* <button className="btn btn-primary">Follow</button>
+                    <button className="btn btn-outline-primary">Message</button> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="card mt-3">
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                  <h6 className="mb-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="feather feather-twitter mr-2 icon-inline text-info"
+                    >
+                      <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+                    </svg>
+                    Twitter
+                  </h6>
+                  <span className="text-secondary">@twitter</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                  <h6 className="mb-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="feather feather-instagram mr-2 icon-inline text-danger"
+                    >
+                      <rect
+                        x="2"
+                        y="2"
+                        width="20"
+                        height="20"
+                        rx="5"
+                        ry="5"
+                      ></rect>
+                      <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                      <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+                    </svg>
+                    Instagram
+                  </h6>
+                  <span className="text-secondary">instagram</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                  <h6 className="mb-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="feather feather-facebook mr-2 icon-inline text-primary"
+                    >
+                      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+                    </svg>
+                    Facebook
+                  </h6>
+                  <span className="text-secondary">Facebook</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-8">
+            <div className="card mb-3">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-sm-3">
+                    <h6 className="mb-0">Full Name</h6>
+                  </div>
+                  <div className="col-sm-9 text-secondary">{fullName}</div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <h6 className="mb-0">Email</h6>
+                  </div>
+                  <div className="col-sm-9 text-secondary">{user.email}</div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <h6 className="mb-0">Phone</h6>
+                  </div>
+                  <div className="col-sm-9 text-secondary">{user.phone}</div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <h6 className="mb-0">Mobile</h6>
+                  </div>
+                  <div className="col-sm-9 text-secondary">{user.phone}</div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-3">
+                    <h6 className="mb-0">Address</h6>
+                  </div>
+                  <div className="col-sm-9 text-secondary">
+                    {user.address}
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-12">
+                    <a
+                      className="btn btn-info "
+                      target="__blank"
+                      href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills"
+                    >
+                      Edit
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row gutters-sm">
+              <div className="col-sm-6 mb-3">
+                <div className="card h-100">
+                  <div className="card-body">
+                    <h6 className="d-flex align-items-center mb-3">Bio</h6>
+                    <p>
+                      Bootstrap 5 edit user profile template snippet is created
+                      by Edgar Burongoh using Bootstrap 5. This snippet is free
+                      and open source hence you can use it in your
+                      project.Bootstrap 5 edit user profile template snippet
+                      example is best for all kind of projects.A great starter
+                      for your new awesome project with 1000+ Font Awesome
+                      Icons, 4000+ Material Design Icons and Material Design
+                      Colors at BBBootstrap.com.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+    }
+}
+
+export default Profile;
