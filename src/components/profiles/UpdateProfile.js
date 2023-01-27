a270b802ed6394155a8817cb537ec21d57c2a8f6
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-01-28 01:37:44 +0300

    added user update funcionality

--- src/components/profiles/UpdateProfile.js
+++ src/components/profiles/UpdateProfile.js
@@ -2,15 +2,19 @@ import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../context";
 
 function UpdateProfile() {
+  const { user } = useContext(AppContext);
   const [userProfile, setUserProfile] = useState({
     first_name: "",
     last_name: "",
+    username: "",
     email: "",
     role: "",
     phone: "",
     address: "",
+    password: "",
+    password_confirmation: '',
   });
-  const { user } = useContext(AppContext);
+
   const token = localStorage.getItem("token");
   useEffect(() => {
     if (user) {
@@ -20,14 +24,41 @@ function UpdateProfile() {
     }
   }, [user]);
 
-  function handleChange(e) {}
-  console.log(userProfile);
+  function handleChange(e) {
+    let name = e.target.name 
+    let value = e.target.value
+
+    setUserProfile({...userProfile, [name]: value})
+  }
+  
+  function handleSubmit(e){
+    e.preventDefault()
+    fetch(`http://localhost:3000/users/${user.id}`,{
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userProfile)
+    }).then(res=>{
+      if(res.ok){
+        res.json().then(data=>{
+
+        })
+      }else{
+        res.json().then(error=>console.log(error))
+      }
+    })
+    console.log(user)
+  }
+  // console.log(userProfile);
   if (user) {
     const fullName = `${
       user.first_name.charAt(0).toUpperCase() + user.first_name.substring(1)
     } ${user.last_name.charAt(0).toUpperCase() + user.last_name.substring(1)}`;
     return (
       <div className="container rounded bg-white mt-5 mb-5">
+        
         <div className="row">
           <div className="col-md-3 border-right">
             <div className="d-flex flex-column align-items-center text-center p-3 py-5">
@@ -41,7 +72,9 @@ function UpdateProfile() {
               <span> </span>
             </div>
           </div>
+          
           <div className="col-md-5 border-right">
+          <form onSubmit={handleSubmit}>
             <div className="p-3 py-5">
               <div className="d-flex justify-content-between align-items-center mb-3">
                 <h4 className="text-right">Profile Settings</h4>
@@ -53,6 +86,7 @@ function UpdateProfile() {
                     type="text"
                     className="form-control"
                     placeholder="first name"
+                    name="first_name"
                     value={userProfile.first_name}
                     onChange={handleChange}
                   />
@@ -63,17 +97,41 @@ function UpdateProfile() {
                     type="text"
                     className="form-control"
                     value={userProfile.last_name}
-                    placeholder="surname"
+                    name="last_name"
+                    placeholder="Last nname"
                     onChange={handleChange}
                   />
                 </div>
               </div>
               <div className="row mt-3">
                 <div className="col-md-12">
+                  <label className="labels">Username</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="username"
+                    placeholder="enter address line 1"
+                    value={userProfile.username}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="col-md-12">
+                  <label className="labels">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="enter address line 1"
+                    name="email"
+                    value={userProfile.email}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="col-md-12">
                   <label className="labels">Mobile Number</label>
                   <input
                     type="text"
                     className="form-control"
+                    name="phone"
                     placeholder="enter phone number"
                     value={userProfile.phone}
                     onChange={handleChange}
@@ -85,6 +143,7 @@ function UpdateProfile() {
                     type="text"
                     className="form-control"
                     placeholder="enter address line 1"
+                    name="address"
                     value={userProfile.address}
                     onChange={handleChange}
                   />
@@ -93,13 +152,15 @@ function UpdateProfile() {
               <div className="mt-5 text-center">
                 <button
                   className="btn btn-primary profile-button"
-                  type="button"
+                  type="submit"
                 >
                   Save Profile
                 </button>
               </div>
             </div>
+            </form>
           </div>
+          
         </div>
       </div>
     );
