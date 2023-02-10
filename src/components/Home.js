f909055b7a9c47ef068fee6ef67e754984b50133
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-10 22:57:49 +0300

    modified screen responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,31 @@
-import React, {useEffect, useContext} from 'react'
-import { AppContext } from '../context'
-import ReviewForm from './reviews/ReviewForm'
-import UserServiceCard from './services/UserServiceCard'
+import React, { useEffect, useContext } from "react";
+import { AppContext } from "../context";
+import ReviewForm from "./reviews/ReviewForm";
+import UserServiceCard from "./services/UserServiceCard";
 
-function Home() { 
-  const {user, services, userServices} = useContext(AppContext)
-  useEffect(()=>{
-    document.title = "Home"
-  })
+function Home() {
+  const { user, services, userServices } = useContext(AppContext);
+  useEffect(() => {
+    document.title = "Home";
+  });
 
-  const serviceCards = userServices.map(service=>{
-    return <UserServiceCard key={service.id} service={service}/>
-  })
+  const serviceCards = userServices.map((service) => {
+    return <UserServiceCard key={service.id} service={service} />;
+  });
   return (
-    <div className='main'>
-      {user ? <h2 className='display-4'>Hello there {user.username}</h2>: <h2>Login</h2>}
-      <h2 className='display-4 center' style={{textAlign: "center"}}>Available Services</h2>
-      <div className='main-content'>
-      {serviceCards}
+    <div className="main">
+      {user ? (
+        <h2 className="display-4">Hello there {user.username}</h2>
+      ) : (
+        <h2>Login</h2>
+      )}
+      <h2 className="display-4 center" style={{ textAlign: "center" }}>
+        Available Services
+      </h2>
+      <div className="main-content">{serviceCards}</div>
+      <ReviewForm />
     </div>
-    <ReviewForm/>
-    </div>
-    
-  )
+  );
 }
 
-export default Home
+export default Home;
