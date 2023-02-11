01b26ee6ce1789a139b6293526b43b7c282408ce
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-11 22:47:57 +0300

    added users and setusers

--- src/context.js
+++ src/context.js
@@ -4,6 +4,7 @@ const AppContext = createContext();
 
 function ContextProvider({children}){
     const [user, setUser] = useState(null);
+    const [users, setUsers] = useState([])
     const [services, setServices] = useState([])
     const [terms, setTerms] = useState([])
     const [userServices, setUserServices] = useState([])
@@ -19,6 +20,8 @@ function ContextProvider({children}){
         setTerms,
         appointmentOrders,
         setAppointmentOrders,
+        users,
+        setUsers
     }
     return(
         <AppContext.Provider value={value}>
